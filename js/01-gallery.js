5b44ae41006f11358c02514c2532b1b08000120a
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-10-26 18:28:29 +0300

    do 2 homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
+// Change code below this line
 console.log(galleryItems);
+
+const boxForGalleryCard = document.querySelector(".gallery");
+
+const galleryCard = galleryItems
+  .map((image) => {
+    const { preview, original, description } = image;
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+    .join("");
+  
+boxForGalleryCard.insertAdjacentHTML("beforeend", galleryCard);
+
+    
+const onImageClick = (event) => {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1200">
+`);
+    
+    instance.show();
+
+    if (instance.visible()) {
+    window.addEventListener("keydown", (event) => {
+            if (event.code === "Escape") {
+             instance.close()
+            } 
+        })
+    }   
+};
+
+
+boxForGalleryCard.addEventListener("click", onImageClick);
