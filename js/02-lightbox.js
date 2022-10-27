0ee44c87f7e399175b9346be3e8375237303a6d4
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-10-27 09:48:51 +0300

    remove consol

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const listOfGalleryCard = document.querySelector(".gallery");
 
 const galleryImage = galleryItems
